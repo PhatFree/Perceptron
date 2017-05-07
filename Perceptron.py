24493235a8f2b804e86462a00e6c1185a30f1537
Jeffrey Booher-Kaeding <jeffbk95@gmail.com>
2017-05-07 14:10:43 -0700

    As far as I can go for now.

---
+++ Perceptron.py
@@ -0,0 +1,70 @@
+#inputs will only contain 1's or 0's
+#weights will contain inputs+1, the extra weight is for the threshold
+
+
+# weight[0] = threshold
+# weight[1] = density
+# weight[2] = symmetry
+# weight[3] = maxium number of line intersections
+sampleweights = [-.1, .2, .3, -.4]
+# dataset[i][0] = density
+# dataset[i][1] = symmetry
+# dataset[i][2] = maxium number of line intersections
+# dataset[i][-1/3] = expected
+sampledataset = [[.4, .2, .3, 0], [1, .53, .99, 1], [.982, 3.1, .23, 1], [.33, 1, 2, 0]]
+
+
+def predict(inputs, weights):
+    # weights[0] is the threashold
+    activation = weights[0]
+    #print("weights:" + str(weights))
+    #print("inputs:" + str(inputs))
+    # Sum wi * Ii
+    for i in range(len(inputs) - 1):
+        activation += weights[i + 1] * inputs[i]
+    return 1.0 if activation > 0.0 else 0.0
+
+
+def train_weights(train, l_rate, n_epoch):
+    weights = [0.0 for i in range(len(train[0]))]
+    # epoch = times to train
+    for epoch in range(n_epoch):
+        sum_error = 0.0
+        # begin training
+
+        for row in train:
+            prediction = predict(row, weights)
+            error = row[-1] - prediction
+            sum_error += error ** 2
+            weights[0] = weights[0] + l_rate * error
+            for i in range(len(row) - 1):
+                weights[i + 1] = weights[i + 1] + l_rate * error * row[i]
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+    return weights
+
+
+
+'''
+def main():
+    dataset = [[2.7810836, 2.550537003, 0],
+               [1.465489372, 2.362125076, 0],
+               [3.396561688, 4.400293529, 0],
+               [1.38807019, 1.850220317, 0],
+               [3.06407232, 3.005305973, 0],
+               [7.627531214, 2.759262235, 1],
+               [5.332441248, 2.088626775, 1],
+               [6.922596716, 1.77106367, 1],
+               [8.675418651, -0.242068655, 1],
+               [7.673756466, 3.508563011, 1]]
+    l_rate = 0.1
+    n_epoch = 10
+    weights = train_weights(dataset, l_rate, n_epoch)
+    print(weights)
+
+    # for row in sampledataset:
+    #   predicton = predict(row,sampleweights)
+    #  print("Expected=%d, Predicted=%d" % (row[-1],predicton))
+
+
+main()
+'''
--- features.py
+++ features.py
@@ -1,11 +1,27 @@
-
+import Perceptron
 
 def main():
     print("CS480:AI Homework 4, Problem 4 - Perceptron Training")
-    raw_data = load_images()
+    raw_data,raw_exprected,ones,fives = load_images()
 
-    for image in raw_data:
-        extract_features(image)
+    print(raw_exprected)
+    raw=[]
+    for i in range(len(raw_data)):
+        if raw_exprected[i] == 5:
+            feature = extract_features(raw_data[i], 0.0)
+        if raw_exprected[i] == 1:
+            feature = extract_features(raw_data[i], 1.0)
+
+        raw.append(feature)
+        print(feature)
+
+    '''
+    we'll have to create two separate percptrons, one for 1's and one for fives.
+    they'll have to be passed a 2d array, with rows being "images" and with cols
+    being the values of the features, and the expected will be feature[-1]
+    '''
+
+    Perceptron.train_weights(raw,0.1,len(raw))
 
     print('done')
 
@@ -18,6 +34,8 @@ def load_images():
         num_ones = 0
         num_fives = 0
 
+        raw = []
+        rawe = []
         ones = []
         fives = []
 
@@ -25,12 +43,19 @@ def load_images():
             if len(images[i]) > 0:
                 if images[i][0] == '1':
                     ones.append(images[i][1:])
+                    raw.append(images[i][1:])
                     num_ones += 1
+                    rawe.append(1)
+
                 if images[i][0] == '5':
                     fives.append(images[i][1:])
+                    raw.append(images[i][1:])
                     num_fives += 1
+                    rawe.append(5)
             else:
                 print('empty image')  # end-of-file
+
+
 
         print(num_ones, 'images of 1')
         print(num_fives, 'images of 5')
@@ -39,35 +64,38 @@ def load_images():
         # convert to matrix form
         one_arrays = [None]*num_ones
         five_arrays = [None]*num_fives
+        raw_arrays = [None]*(num_fives+num_ones)
         for i in range(len(ones)):
             one_arrays[i] = [[int(x) for x in line.split()] for line in ones[i].split('\n')]
         for i in range(len(fives)):
             five_arrays[i] = [[int(x) for x in line.split()] for line in fives[i].split('\n')]
+        for i in range(len(raw)):
+            raw_arrays[i] = [[int(x) for x in line.split()] for line in raw[i].split('\n')]
 
     print('images loaded')
-    return one_arrays
+    return raw_arrays,rawe,one_arrays,five_arrays
 
 
 
-def extract_features(image):
+def extract_features(image,expected):
     tpose = list(zip(*image))  # transpose image for easier vertical analysis
 
     density = sum(sum(row) for row in image) / (sum(len(row) for row in image))
 
-    symmetry_horz = 0
-    symmetry_vert = 0
+    symmetry_horz = 0.0
+    symmetry_vert = 0.0
 
     #
 
-    intersect_horz_min = 0
+    intersect_horz_min = 0.0
 
-    intersect_horz_max = 0
+    intersect_horz_max = 0.0
 
-    intersect_vert_min = 0
-    intersect_vert_max = 0
+    intersect_vert_min = 0.0
+    intersect_vert_max = 0.0
 
     features = [density, symmetry_vert, symmetry_horz,
-                intersect_horz_min, intersect_horz_max, intersect_vert_min, intersect_vert_max]
+                intersect_horz_min, intersect_horz_max, intersect_vert_min, intersect_vert_max,expected]
     return features
 
 
